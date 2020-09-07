6a2e84bae927a39e6d8ebf951f8f68265b23c450
chenhong <chenhong03@baijiahulian.com>
2020-09-07 14:21:54 +0800

    开发番茄钟demo

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # study-geektime-electron
-这是学习极客时间的electron，跟着练习的demo
+这是学习极客时间的electron，跟着练习的demo-番茄钟 demo 完整版
+
+# 运行
+
+```
+    npm install
+    npm start
+```
---
+++ main.js
@@ -0,0 +1,51 @@
+const {app, BrowserWindow, ipcMain, Notification} = require('electron');
+
+let mainWindow;
+
+function handleIPC() {
+    ipcMain.handle('notification', async (e, {title, body, actions, closeButtonText}) => {
+        let res = await new Promise((resolve, reject) => {
+            console.log({title, body, actions, closeButtonText});
+
+            let notification = new Notification({
+                title,
+                body,
+                actions: [{text: '确定', type: 'button'}],
+                closeButtonText: '取消'
+            });
+
+            notification.show();
+            notification.on('action', function(event) {
+                resolve({
+                    event: 'action'
+                })
+            })
+            notification.on('close', function(event) {
+                resolve({
+                    event: 'close'
+                })
+            })
+        });
+
+        return res;
+    })
+}
+
+function createMainWindow() {
+    mainWindow = new BrowserWindow({
+        width: 250,
+        height: 350,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    })
+    mainWindow.loadFile('./index.html')
+
+    return mainWindow
+}
+
+app.whenReady().then(() => {
+    handleIPC()
+    createMainWindow()
+})
+
